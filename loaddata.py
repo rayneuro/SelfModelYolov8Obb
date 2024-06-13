90c75a41ec0ae37bbe3cb9a5b074dcc580190c26
ray <ray1997ray75@gmail.com>
2024-06-13 23:50:57 +0800

    Jun 13

--- loaddata.py
+++ loaddata.py
@@ -9,9 +9,61 @@ from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
 from typing import List
 from augment import * 
+from dataset import *
 
 
 
+def colorstr(*input):
+    """
+    Colors a string based on the provided color and style arguments. Utilizes ANSI escape codes.
+    See https://en.wikipedia.org/wiki/ANSI_escape_code for more details.
+
+    This function can be called in two ways:
+        - colorstr('color', 'style', 'your string')
+        - colorstr('your string')
+
+    In the second form, 'blue' and 'bold' will be applied by default.
+
+    Args:
+        *input (str): A sequence of strings where the first n-1 strings are color and style arguments,
+                      and the last string is the one to be colored.
+
+    Supported Colors and Styles:
+        Basic Colors: 'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'
+        Bright Colors: 'bright_black', 'bright_red', 'bright_green', 'bright_yellow',
+                       'bright_blue', 'bright_magenta', 'bright_cyan', 'bright_white'
+        Misc: 'end', 'bold', 'underline'
+
+    Returns:
+        (str): The input string wrapped with ANSI escape codes for the specified color and style.
+
+    Examples:
+        >>> colorstr("blue", "bold", "hello world")
+        >>> "\033[34m\033[1mhello world\033[0m"
+    """
+    *args, string = input if len(input) > 1 else ("blue", "bold", input[0])  # color arguments, string
+    colors = {
+        "black": "\033[30m",  # basic colors
+        "red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "blue": "\033[34m",
+        "magenta": "\033[35m",
+        "cyan": "\033[36m",
+        "white": "\033[37m",
+        "bright_black": "\033[90m",  # bright colors
+        "bright_red": "\033[91m",
+        "bright_green": "\033[92m",
+        "bright_yellow": "\033[93m",
+        "bright_blue": "\033[94m",
+        "bright_magenta": "\033[95m",
+        "bright_cyan": "\033[96m",
+        "bright_white": "\033[97m",
+        "end": "\033[0m",  # misc
+        "bold": "\033[1m",
+        "underline": "\033[4m",
+    }
+    return "".join(colors[x] for x in args) + f"{string}" + colors["end"]
 
 
 def build_yolo_dataset(cfg, img_path, batch, data, mode="train", rect=False, stride=32, multi_modal=False):
@@ -34,45 +86,6 @@ def build_yolo_dataset(cfg, img_path, batch, data, mode="train", rect=False, str
         data=data,
         fraction=cfg.fraction if mode == "train" else 1.0,
     )
-
-
-
-
-
-class Yolov8obbDataset(Dataset) : 
-    def __init__(self, image_folder, label_folder, nc = 3, transform = None):
-        self.image_folder = image_folder
-        self.label_folder = label_folder
-        self.transform = transform
-        self.images = load_images_from_folder(self.image_folder)
-        self.labels = get_label_from_folder(self.label_folder)
-        
-    def __len__(self):
-        return len(self.images)
-    
-    def __getitem__(self, idx):
-        #load images and bounding boxes label
-
-        label = deepcopy(self.labels[idx])
-        cls = deepcopy(label[0])
-        image = self.images[idx]
-        
-        #if self.transform:
-        #image = self.transform(image)
-
-        target = {}
-        target['boxes'] = torch.tensor([labels[1:7]],dtype = torch.float32)
-        target['labels'] = torch.tensor([cls],dtype = torch.int64)
-        target['h'] =  torch.tensor(image.shape[2],dtype = torch.int64)
-        target['w'] =  torch.tensor(image.shape[1],dtype = torch.int64)
-        
-        return image, target
-    
-    
-    def collate_fn(batch):
-        return tuple(zip(*batch))
-
-
 
 
 
